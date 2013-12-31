c60910c4abc1c11195fb1be508639c7f9d6ca4f5
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2013-12-31 00:58:38 -0500

    added message group test

--- src/test/java/org/f1x/v1/Test_ByteBufferMessageBuilder.java
+++ src/test/java/org/f1x/v1/Test_ByteBufferMessageBuilder.java
@@ -12,6 +12,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.v1;
 
 import org.junit.Assert;
@@ -61,6 +75,23 @@ public class Test_ByteBufferMessageBuilder {
         mb.addRaw(FixTags.EncodedText, helloEncoded, 0, helloEncoded.length);
 
         assertContentEquals("347=SHIFT_JIS|58=Hello|354=10|355=\ufffd\ufffd\ufffd\ufffd\u0242\ufffd\ufffd\ufffd|");
+    }
+
+    @Test
+    public void testRepeatingGroup () throws UnsupportedEncodingException {
+        mb.add(FixTags.NoMDEntries, 2);
+
+        mb.add(FixTags.MDEntryType, MDEntryType.BID);
+        mb.add(FixTags.MDEntryPx, 12.32);
+        mb.add(FixTags.MDEntrySize, 100);
+        mb.add(FixTags.QuoteEntryID, "BID123");
+
+        mb.add(FixTags.MDEntryType, MDEntryType.OFFER);
+        mb.add(FixTags.MDEntryPx, 12.32);
+        mb.add(FixTags.MDEntrySize, 100);
+        mb.add(FixTags.QuoteEntryID, "OFFER123");
+
+        assertContentEquals("268=2|269=0|270=12.32|271=100|299=BID123|269=1|270=12.32|271=100|299=OFFER123|");
     }
 
     @Test
