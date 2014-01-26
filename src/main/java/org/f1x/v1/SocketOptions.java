dbc0d45681ecc17becf0f3e1f33695be95907183
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-01-26 17:23:40 -0500

    Added setters

--- src/main/java/org/f1x/v1/SocketOptions.java
+++ src/main/java/org/f1x/v1/SocketOptions.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.v1;
 
 public class SocketOptions {
@@ -11,6 +25,15 @@ public class SocketOptions {
         return isSocketKeepAlive;
     }
 
+    public void setSocketKeepAlive(boolean socketKeepAlive) {
+        isSocketKeepAlive = socketKeepAlive;
+    }
+
+    public void setSocketTcpNoDelay(boolean socketTcpNoDelay) {
+        isSocketTcpNoDelay = socketTcpNoDelay;
+    }
+
+
     public boolean isSocketTcpNoDelay() {
         return isSocketTcpNoDelay;
     }
@@ -19,9 +42,18 @@ public class SocketOptions {
         return socketRecvBufferSize;
     }
 
+    public void setSocketRecvBufferSize(int socketRecvBufferSize) {
+        this.socketRecvBufferSize = socketRecvBufferSize;
+    }
+
     public int getSocketSendBufferSize() {
         return socketSendBufferSize;
     }
+
+    public void setSocketSendBufferSize(int socketSendBufferSize) {
+        this.socketSendBufferSize = socketSendBufferSize;
+    }
+
 
     /** @return Specifies SO_TIMEOUT in milliseconds, zero means infinity */
     public int getSocketTimeout() {
