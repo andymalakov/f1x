647481eecf6718e888e18aefc4bfb66c1dde4b23
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-01-28 17:13:46 -0500

    Setting Default Connection Interceptor

--- src/main/java/org/f1x/v1/FixSocketCommunicator.java
+++ src/main/java/org/f1x/v1/FixSocketCommunicator.java
@@ -26,6 +26,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.v1;
 
 import org.f1x.api.FixSettings;
@@ -36,6 +50,7 @@ import org.f1x.io.InputStreamChannel;
 import org.f1x.io.OutputChannel;
 import org.f1x.io.OutputStreamChannel;
 import org.f1x.io.socket.ConnectionInterceptor;
+import org.f1x.io.socket.DefaultConnectionInterceptor;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -43,7 +58,7 @@ import java.net.SocketAddress;
 
 public abstract class FixSocketCommunicator extends FixCommunicator {
 
-    private ConnectionInterceptor connectionInterceptor;
+    private ConnectionInterceptor connectionInterceptor = new DefaultConnectionInterceptor();
 
     public FixSocketCommunicator(FixVersion fixVersion, FixSettings settings) {
         super(fixVersion, settings);
