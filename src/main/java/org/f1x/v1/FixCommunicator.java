5726842dcefa5af93db9e45463c5a13502723221
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-01-27 20:00:48 -0500

    acceptor now echoes inbound tag 141, moved AdminMessageTypes enum, other minor stuff

--- src/main/java/org/f1x/v1/FixCommunicator.java
+++ src/main/java/org/f1x/v1/FixCommunicator.java
@@ -40,6 +40,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.v1;
 
 import org.f1x.api.FixSettings;
@@ -63,7 +77,6 @@ import org.f1x.io.OutputChannel;
 import org.f1x.log.MessageLog;
 import org.f1x.log.MessageLogFactory;
 import org.f1x.log.file.FileMessageLogFactory;
-import org.f1x.tools.AdminMessageTypes;
 import org.f1x.util.AsciiUtils;
 import org.f1x.util.ByteArrayReference;
 import org.f1x.util.RealTimeSource;
@@ -597,11 +610,13 @@ public abstract class FixCommunicator implements FixSession {
 //            else
 //                resend?
 //        }
-        processInboundLogon();
+        processInboundLogon(isSequenceNumberReset);
     }
 
-    /** Handle inbound LOGON message depending on FIX session role (acceptor/initator) and current state */
-    protected abstract void processInboundLogon() throws IOException;
+    /** Handle inbound LOGON message depending on FIX session role (acceptor/initator) and current state
+     * @param isSequenceNumberReset true if inbound LOGON message contains tag ResetSeqNum(141)=Y.
+     */
+    protected abstract void processInboundLogon(boolean isSequenceNumberReset) throws IOException;
 
     @SuppressWarnings("unused")
     protected void processInboundLogout(MessageParser parser) throws IOException {
--- src/test/java/org/f1x/log/file/Test_FileMessageLog.java
+++ src/test/java/org/f1x/log/file/Test_FileMessageLog.java
@@ -40,15 +40,32 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.log.file;
 
 import org.f1x.SessionIDBean;
 import org.f1x.util.AsciiUtils;
 import org.f1x.util.StoredTimeSource;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Arrays;
 
 public class Test_FileMessageLog {
@@ -59,19 +76,26 @@ public class Test_FileMessageLog {
     private static final byte[] INBOUND = AsciiUtils.getBytes("8=FIX.4.4\u00019=65\u000135=1\u000134=5\u000149=SERVER\u000152=20131223-15:41:22.154\u000156=CLIENT\u0001112=TEST\u000110=159\u0001");
     private static final byte[] OUTBOUND = AsciiUtils.getBytes("8=FIX.4.4\u00019=65\u000135=0\u000134=2\u000149=CLIENT\u000152=20131223-15:41:17.888\u000156=SERVER\u0001112=TEST\u000110=173\u0001");
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void testLoggingFormat() throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        FileMessageLog log = new FileMessageLog(baos, new SessionIDBean("TEST", "TEST"), StoredTimeSource.makeFromUTCTimestamp(DATE+'-'+TIME));
 
+        File dir = folder.newFolder("log");
+        SessionIDBean sessionID = new SessionIDBean("TEST", "TEST");
+        FileMessageLog log = new FileMessageLog(dir, sessionID, StoredTimeSource.makeFromUTCTimestamp(DATE+'-'+TIME), 5000);
+
+
+        File logFile = new File (dir, log.encodeAsLogFilename(sessionID));
         log.logInbound(wrap(INBOUND,2), 2, OUTBOUND.length);
         log.logOutbound(wrap(OUTBOUND,2), 2, OUTBOUND.length);
 
         log.close();
 
-        LineNumberReader reader = new LineNumberReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray())));
-        Assert.assertEquals(TIME + ' ' + new String(FileMessageLog.IN) + ' ' +  new String (INBOUND), reader.readLine());
-        Assert.assertEquals(TIME + ' ' + new String(FileMessageLog.OUT) + ' ' +  new String (OUTBOUND), reader.readLine());
+        LineNumberReader reader = new LineNumberReader(new FileReader(logFile));
+        Assert.assertEquals(TIME + " IN > " +  new String (INBOUND), reader.readLine());
+        Assert.assertEquals(TIME + " OUT> " +  new String (OUTBOUND), reader.readLine());
     }
 
     private static byte[] wrap(byte[] arr, int wrapperSize) {
