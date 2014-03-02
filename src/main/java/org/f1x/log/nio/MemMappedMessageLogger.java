98bfbc42d7050945a001c1e60fbf2c12ba81c7ae
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-03-01 22:50:09 -0500

    Fixed size logger backed by memory mapped file

--- src/main/java/org/f1x/log/nio/MemMappedMessageLogger.java
+++ src/main/java/org/f1x/log/nio/MemMappedMessageLogger.java
@@ -26,6 +26,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.log.nio;
 
 import org.f1x.log.MessageLog;
@@ -38,34 +52,46 @@ import java.nio.channels.FileChannel;
 
 /**
  * Message logger that keeps last <code>maxSize</code> bytes of FIX traffic.
+ * Fixed size logger backed by memory mapped file.
  */
 public class MemMappedMessageLogger implements MessageLog {
+
+    private static final byte IN = (byte) '>';
+    private static final byte OUT = (byte) '<';
+
+    static final byte [] EOF =  { (byte)'E', (byte)'O', (byte)'F'};
+    static final byte [] TAIL = { (byte)' ', (byte)' ', (byte)' '};
 
     private final RandomAccessFile raf;
     private final MappedByteBuffer out;
 
     public MemMappedMessageLogger (File file, int maxSize) {
+        assert EOF.length == TAIL.length; // we use TAIL to wipe "EOF"
+
         try {
             raf = new RandomAccessFile(file, "rw");
             out = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, maxSize);
+
+            out.put(EOF, 0, EOF.length);
+
         } catch (IOException e) {
             throw new RuntimeException("Error opening log file " + file.getAbsolutePath(), e);
         }
     }
 
-    private static final byte IN = (byte) '>';
-    private static final byte OUT = (byte) '<';
-    private static final byte [] EOF = { (byte)'\n', (byte)'E', (byte)'O', (byte)'F'};
-
     @Override
     public void log(boolean isInbound, byte[] buffer, int offset, int length) {
-        if (out.remaining() < length)
+
+        out.position(out.position() - EOF.length);  // step back
+
+        if (out.remaining() < length + EOF.length + 1) {  //TODO: + EOF.length + 1 ?
+            out.put(TAIL, 0, TAIL.length);
             out.position(0);
-        else
-            out.position(out.position() - EOF.length - 1);  // step back
+        }
 
         out.put(isInbound ? IN : OUT);
         out.put(buffer, offset, length);
+        out.put((byte)'\n');
         out.put(EOF, 0, EOF.length);
 
     }
