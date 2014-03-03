df4839b6380c0b11e61eb44903d8c7d371788efc
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-03-02 20:47:12 -0500

    Plugged new file loggers into default log factory

--- src/main/java/org/f1x/log/file/FileMessageLogFactory.java
+++ src/main/java/org/f1x/log/file/FileMessageLogFactory.java
@@ -68,16 +68,32 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.log.file;
 
 import org.f1x.api.session.SessionID;
 import org.f1x.log.MessageLog;
 import org.f1x.log.MessageLogFactory;
 import org.f1x.log.OutputStreamMessageLog;
+import org.f1x.log.file.nio.MemMappedMessageLogger;
 import org.f1x.util.RealTimeSource;
 
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.TimeZone;
 
 public class FileMessageLogFactory implements MessageLogFactory {
 
@@ -100,30 +116,15 @@ public class FileMessageLogFactory implements MessageLogFactory {
 
 
     @Override
-    public MessageLog create(final SessionID sessionID) {
+    public MessageLog create(SessionID sessionID) {
 
-        //TODO: Deprecate SimpleFileMessageLog ?
-        if (true)
-            return new SimpleFileMessageLog (getLogFile(sessionID, 0), RealTimeSource.INSTANCE, logSettings.getFileBufferSize());
+        if (logSettings.getMaxLogSize() > 0)
+            return new MemMappedMessageLogger(getLogFile(sessionID), logSettings.getMaxLogSize());
 
-        if (logSettings.getMaxLogSize() > 0) {
-            // Should this also do a periodic flush?
-            RollingFileMessageLog.OutputStreamFactory streamFactory = new RollingFileMessageLog.OutputStreamFactory(){
-                private int fileNumber = 1;
+        if (logSettings.isDaily())
+            return new DailyFileMessageLog(sessionID, dir, RealTimeSource.INSTANCE, TimeZone.getDefault(), logSettings.getFileBufferSize(), logSettings.getFlushPeriod());
 
-                @Override
-                public OutputStream create() {
-                    return getLogOutputStream (sessionID, fileNumber++);
-                }
-            };
-
-            RollingFileMessageLog result = new RollingFileMessageLog(streamFactory, sessionID, RealTimeSource.INSTANCE, logSettings.getFlushPeriod(), logSettings.getMaxLogSize());
-            result.start();
-            return result;
-        }
-
-
-        OutputStream os = getLogOutputStream(sessionID, 0);
+        OutputStream os = getLogOutputStream(sessionID);
 
         if (logSettings.getFlushPeriod() > 0) {
             PeriodicFlushingMessageLog result = new PeriodicFlushingMessageLog(os, sessionID, RealTimeSource.INSTANCE, logSettings.getFlushPeriod());
@@ -134,9 +135,9 @@ public class FileMessageLogFactory implements MessageLogFactory {
         return new OutputStreamMessageLog(os, RealTimeSource.INSTANCE);
     }
 
-    private OutputStream getLogOutputStream (SessionID sessionID, int fileNumber) {
+    private OutputStream getLogOutputStream (SessionID sessionID) {
         try {
-            File file = getLogFile(sessionID, fileNumber);
+            File file = getLogFile(sessionID);
             OutputStream os = new FileOutputStream(file, true); // append
             if (logSettings.getFileBufferSize() > 0)
                 os = new BufferedOutputStream(os, logSettings.getFileBufferSize());
@@ -146,19 +147,15 @@ public class FileMessageLogFactory implements MessageLogFactory {
         }
     }
 
-    private File getLogFile (SessionID sessionID, int fileNumber) {
-        return new File (dir, encodeAsLogFilename(sessionID, fileNumber));
+    private File getLogFile (SessionID sessionID) {
+        return new File (dir, encodeAsLogFilename(sessionID));
     }
 
-    private static String encodeAsLogFilename(SessionID sessionID, int fileNumber) {
+    private static String encodeAsLogFilename(SessionID sessionID) {
         StringBuilder sb = new StringBuilder();
         sb.append (sessionID.getSenderCompId().toString());
         sb.append ('-');
         sb.append (sessionID.getTargetCompId().toString());
-        if (fileNumber > 0) {
-            sb.append('.');
-            sb.append(fileNumber);
-        }
         sb.append(".log");
 
         try {
--- src/main/java/org/f1x/log/file/SimpleFileMessageLog.java
+++ src/main/java/org/f1x/log/file/SimpleFileMessageLog.java
@@ -53,6 +53,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.f1x.log.file;
 
 import org.f1x.log.LogFormatter;
@@ -63,6 +77,7 @@ import org.f1x.util.TimeSource;
 import java.io.*;
 
 /** Simple message log backed by Buffered File Output Stream */
+@Deprecated
 public class SimpleFileMessageLog extends OutputStreamMessageLog {
 
     /**
