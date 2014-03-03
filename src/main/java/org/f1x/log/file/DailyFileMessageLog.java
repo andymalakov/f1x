7bbd89a033bad89cb65af63c761033cceb0759bd
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-03-02 19:55:11 -0500

    minor: daily rollover thread name/priority

--- src/main/java/org/f1x/log/file/DailyFileMessageLog.java
+++ src/main/java/org/f1x/log/file/DailyFileMessageLog.java
@@ -26,6 +26,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.log.file;
 
 import org.f1x.api.session.SessionID;
@@ -74,6 +88,9 @@ public class DailyFileMessageLog extends PeriodicFlushingMessageLog {
 
 
         OutputStreamRollover(File logDir, SessionID sessionID, TimeSource timeSource, TimeZone tz, int bufferSize) {
+            super("Daily roll for " + sessionID);
+            setDaemon(true);
+            setPriority(Thread.NORM_PRIORITY - 1);
             this.logDir = logDir;
             this.bufferSize = bufferSize;
             this.sessionID = sessionID;
