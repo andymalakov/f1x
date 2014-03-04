1f209ccbfa7a4ebd6708ed116226ffba4007368a
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-03-03 22:02:00 -0500

    added synchronization

--- src/main/java/org/f1x/log/file/nio/MemMappedMessageLogger.java
+++ src/main/java/org/f1x/log/file/nio/MemMappedMessageLogger.java
@@ -54,6 +54,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.f1x.log.file.nio;
 
 import org.f1x.log.MessageLog;
@@ -63,6 +77,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 /**
  * Message logger that keeps last <code>maxSize</code> bytes of FIX traffic.
@@ -86,7 +101,12 @@ public class MemMappedMessageLogger implements MessageLog {
             raf = new RandomAccessFile(file, "rw");
             out = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, maxSize);
 
+//            if (out.hasArray())
+//                Arrays.fill(out.array(), (byte)' ');
+
             out.put(EOF, 0, EOF.length);
+
+
 
         } catch (IOException e) {
             throw new RuntimeException("Error opening log file " + file.getAbsolutePath(), e);
@@ -94,11 +114,11 @@ public class MemMappedMessageLogger implements MessageLog {
     }
 
     @Override
-    public void log(boolean isInbound, byte[] buffer, int offset, int length) {
-
+    public synchronized void log(boolean isInbound, byte[] buffer, int offset, int length) {
+        //TODO: bug may produce negative
         out.position(out.position() - EOF.length);  // step back
 
-        if (out.remaining() < length + EOF.length + 1) {  //TODO: + EOF.length + 1 ?
+        if (out.remaining() < length + EOF.length + 1) {
             out.put(TAIL, 0, TAIL.length);
             out.position(0);
         }
@@ -111,7 +131,7 @@ public class MemMappedMessageLogger implements MessageLog {
     }
 
     @Override
-    public void close() throws IOException {
+    public synchronized void close() throws IOException {
         out.force();
         raf.close();
     }
