bb408ea30a3a1f55e61ba5b484e2f10d886a6119
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-03-12 23:52:14 -0400

    Rolling file logger

--- src/main/java/org/f1x/log/file/PeriodicFlushingMessageLogFactory.java
+++ src/main/java/org/f1x/log/file/PeriodicFlushingMessageLogFactory.java
@@ -25,102 +25,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.f1x.log.file;
 
 import org.f1x.api.session.SessionID;
 import org.f1x.log.LogFormatter;
 import org.f1x.log.MessageLog;
-import org.f1x.log.MessageLogFactory;
 import org.f1x.log.SimpleLogFormatter;
 import org.f1x.util.RealTimeSource;
 import org.f1x.util.TimeSource;
 
 import java.io.File;
-import java.util.TimeZone;
 
 /**
- * Factory for {@link DailyFileMessageLog}.
+ * Factory for {@link PeriodicFlushingMessageLog}.
  */
 public class PeriodicFlushingMessageLogFactory extends AbstractFileMessageLogFactory {
     public static final int DEFAULT_FILE_BUFFER_SIZE = 8192;
     public static final int DEFAULT_FLUSH_PERIOD = 15000;
 
-    private final FileNameGenerator fileNameGenerator;
-    private final OutputStreamFactory streamFactory;
+    protected FileNameGenerator fileNameGenerator;
+    protected OutputStreamFactory streamFactory;
 
-    private int flushPeriod = DEFAULT_FLUSH_PERIOD;
-    private TimeSource timeSource = RealTimeSource.INSTANCE;
-    private LogFormatter logFormatter;
+    protected int flushPeriod = DEFAULT_FLUSH_PERIOD;
+    protected TimeSource timeSource = RealTimeSource.INSTANCE;
+    protected LogFormatter logFormatter;
 
     /**
      * @param logDir directory where log files will reside
@@ -174,5 +102,21 @@ public class PeriodicFlushingMessageLogFactory extends AbstractFileMessageLogFac
     /** @param flushPeriod This setting determines how often logger flushes the buffer (in milliseconds). Negative or zero value disables periodic flushing */
     public void setFlushPeriod(int flushPeriod) {
         this.flushPeriod = flushPeriod;
+    }
+
+    public FileNameGenerator getFileNameGenerator() {
+        return fileNameGenerator;
+    }
+
+    public void setFileNameGenerator(FileNameGenerator fileNameGenerator) {
+        this.fileNameGenerator = fileNameGenerator;
+    }
+
+    public OutputStreamFactory getStreamFactory() {
+        return streamFactory;
+    }
+
+    public void setStreamFactory(OutputStreamFactory streamFactory) {
+        this.streamFactory = streamFactory;
     }
 }
--- src/main/java/org/f1x/log/file/RollingFileMessageLog.java
+++ src/main/java/org/f1x/log/file/RollingFileMessageLog.java
@@ -25,79 +25,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.f1x.log.file;
 
 import org.f1x.api.session.SessionID;
+import org.f1x.log.LogFormatter;
 import org.f1x.util.TimeSource;
 
 import java.io.*;
@@ -105,29 +36,38 @@ import java.io.*;
 /**
  * Start a new log file when size of current fle exceeds given maximum.
  */
-@Deprecated ///TODO Add factory
 public class RollingFileMessageLog extends PeriodicFlushingMessageLog {
 
-    private final OutputStreamFactory outputStreamFactory;
+    private final OutputStreamFactory streamFactory;
     private final long bytesPerFile;
     private volatile long bytesWritten; // updated under OutputStreamMessageLog.lock
 
-    public interface OutputStreamFactory {
-        OutputStream create();
+    private final File [] logFiles;
+
+    /**
+     * @param sessionID identifies session for this log
+     * @param timeSource time source used for formatting timestamps
+     * @param bytesPerFile Logger will roll to the next file once current log file size exceeds this limit (in bytes).
+     * @param flushPeriod  This setting determines how often logger flushes the buffer (in milliseconds). Negative or zero value disables periodic flushing.
+     */
+    public RollingFileMessageLog(File [] logFiles, OutputStreamFactory streamFactory, SessionID sessionID, TimeSource timeSource, long bytesPerFile, int flushPeriod) {
+        super(streamFactory.create(logFiles[0]), sessionID, timeSource, flushPeriod);
+
+        this.logFiles = logFiles;
+        this.streamFactory = streamFactory;
+        this.bytesPerFile = bytesPerFile;
     }
 
     /**
-     *
-     * @param outputStreamFactory
-     * @param sessionID
-     * @param timeSource
-     * @param bytesPerFile This setting defines maximum size of each log file.
-     * @param flushPeriod  This setting determines how often logger flushes the buffer (in milliseconds). Negative or zero value disables periodic flushing.
+     * @param sessionID identifies session for this log
+     * @param timeSource time source used for formatting timestamps
+     * @param bytesPerFile Logger will roll to the next file once current log file size exceeds this limit (in bytes).
+     * @param flushPeriod This setting determines how often logger flushes the buffer (in milliseconds). Negative or zero value disables periodic flushing.
      */
-    public RollingFileMessageLog(OutputStreamFactory outputStreamFactory, SessionID sessionID, TimeSource timeSource, long bytesPerFile, int flushPeriod) {
-        super(outputStreamFactory.create(), sessionID, timeSource, flushPeriod);
-
-        this.outputStreamFactory = outputStreamFactory;
+    public RollingFileMessageLog(File [] logFiles, OutputStreamFactory streamFactory, SessionID sessionID, LogFormatter formatter, TimeSource timeSource, long bytesPerFile, int flushPeriod) {
+        super(streamFactory.create(logFiles[0]), sessionID, formatter, timeSource, flushPeriod);
+        this.logFiles = logFiles;
+        this.streamFactory = streamFactory;
         this.bytesPerFile = bytesPerFile;
     }
 
@@ -147,16 +87,13 @@ public class RollingFileMessageLog extends PeriodicFlushingMessageLog {
         }
     }
 
-    protected OutputStream nextOutputStream() {
-        return outputStreamFactory.create();
-    }
-
     protected Flusher createFlusher(SessionID sessionID, TimeSource timeSource, int flushPeriod) {
         return new RollingFlusher(sessionID, timeSource, flushPeriod);
     }
 
 
     private class RollingFlusher extends Flusher {
+        private int fileIndex = 0;
         private long bytesBeforeNextFile = bytesPerFile;
 
         protected RollingFlusher(SessionID sessionID, TimeSource timeSource, int flushPeriod) {
@@ -174,6 +111,11 @@ public class RollingFileMessageLog extends PeriodicFlushingMessageLog {
                 safeClose(prev);
                 bytesBeforeNextFile += bytesPerFile;
             }
+        }
+
+        private OutputStream nextOutputStream() {
+            File nextFile = logFiles[++fileIndex % logFiles.length];  // accessed only by Flusher thread
+            return streamFactory.create(nextFile);
         }
     }
 }
