abdc17856a6c3eb6e0f81f62ef594f7aa710fc34
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-06-12 11:29:46 -0400

    Polished logging

--- src/main/java/org/f1x/v1/SessionEndTask.java
+++ src/main/java/org/f1x/v1/SessionEndTask.java
@@ -1,6 +1,8 @@
 package org.f1x.v1;
 
 import org.f1x.api.session.FixSession;
+import org.gflogger.GFLog;
+import org.gflogger.GFLogFactory;
 
 import java.util.TimerTask;
 
@@ -15,7 +17,8 @@ final class SessionEndTask extends TimerTask {
     @Override
     public void run() {
         try {
-            session.logout("Ending Session");
+            FixCommunicator.LOGGER.info().append("Scheduled end time for FIX session ").append(session.getSessionID()).commit();
+            session.logout("Scheduled end time");
         } catch (Throwable e) {
             FixCommunicator.LOGGER.warn().append("Error occurred during ending session").commit();
         }
